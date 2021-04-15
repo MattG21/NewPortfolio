b01c7d712ef86aa57afb5a6297a749f39fa95192
Matthew G <mgagnon1@live.com>
2021-04-15 16:41:00 -0400

    Changed email
    changed email because it will not work

--- api/routes/email.js
+++ api/routes/email.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const path = require('path');
-const app = express(),
-      bodyParser = require("body-parser");
-      port = 80;
+const app = express();
+const mandrill = require('node-mandrill')('<your API Key>'); 
 
-app.post('/sendEmail', (req,res) => {
-  console.log("Test send email")
+app.get('/sendEmail', (req,res) => {
+  mandrill('/messages/send', {
+      message: {
+          to: [{email: 'mgnon@bu.edu', name: 'Matthew Gagnon'}],
+          from_email: 'mgnon@bu.edu',
+          subject: "Hey, what's up?",
+          text: "Hello, I sent this message using mandrill."
+      }
+  }, function(error, response)
+  {
+      //uh oh, there was an error
+      if (error) {
+        console.log( JSON.stringify(error) );
+      }
+      //everything's good, lets see what mandrill said
+      else { 
+        console.log(response)
+      }
+  });
+
+  // this will not work
 })
+
 
 module.exports = app;
